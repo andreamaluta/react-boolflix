7743a56cfb7c91a158e920c5249963d4ad35c741
andreamaluta <andreamaluta@gmail.com>
2025-05-20 12:43:45 +0200

    [UPD] made the first axios call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import axios from 'axios'
 import { useState } from 'react'
 
 
 function App() {
+
+  const [film, setFilm] = useState();
+
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=ede7653811e5ebe2dc037015dffde9ba&query=ritorno+al+futuro'
+
+  const filmCall = (url) => {
+    axios.get(url).then((res) => console.log(res.data.results))
+
+  }
 
   return (
     <>
@@ -9,6 +19,9 @@ function App() {
         <input type="text" />
         <button>Invia</button>
       </header>
+
+      {filmCall(url)}
+
     </>
   )
 }
