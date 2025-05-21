9c97356c3ccc09e15e2f458b850cc71493f6a50e
andreamaluta <andreamaluta@gmail.com>
2025-05-21 13:34:55 +0200

    [UPD] made the header

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -19,10 +19,25 @@ const FilterForm = () => {
     return (
         <div>
             <header>
-                <form onSubmit={submit}>
-                    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-                    <button>Invia</button>
-                </form>
+                <div className="container">
+                    <div className="row header-row">
+                        <div className="col-4">
+                            <div className="logo">
+                                <img src="src\assets\img\Netflix_Logo_RGB.png" alt="" />
+                            </div>
+                        </div>
+                        <div className="col-8">
+                            <form onSubmit={submit}>
+                                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className='form-control' placeholder='Search' />
+                                <button className='btn btn-outline-light ms-3 '>Invia</button>
+                                <div className="account">
+
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
             </header>
 
         </div>
