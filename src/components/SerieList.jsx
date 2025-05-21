8b25e942f25f55b1a91d52168eceb494e6fd3a86
andreamaluta <andreamaluta@gmail.com>
2025-05-21 14:19:24 +0200

    [UPD] made the card for the series

--- src/components/SerieList.jsx
+++ src/components/SerieList.jsx
@@ -12,20 +12,34 @@ const SerieList = () => {
     const { serie } = useFilm();
 
     const createList = () => {
-        return <ul>
-            {serie.map((item) => (
-                <li key={item.id}>
-                    <img src={createLink(item.poster_path)} alt="" />
-                    <br />
-                    {item.name}
-                    <br />
-                    {item.original_name}
-                    <br />
-                    {languageToFlag(item.original_language)}
-                    < br />
-                    <Stars vote={item.vote_average} />
-                </li>
-            ))}
+        return <ul className='list-unstyled'>
+            <div className="container">
+                <div className="row">
+                    {serie.map((item) => (
+                        <div className="col-2 mb-3">
+                            <div className="card">
+                                <li key={item.id}>
+                                    <img src={createLink(item.poster_path)} alt="" className='card-img-top' />
+                                    <div className="card-body">
+                                        <div>
+                                            <h5>Titolo:</h5> {item.name}
+                                        </div>
+                                        <div>
+                                            <h5>Titolo originale:</h5>{item.original_name}
+                                        </div>
+                                        <div>
+                                            Lingua: {languageToFlag(item.original_language)}
+                                        </div>
+                                        <div>
+                                            Valutazione: <Stars vote={item.vote_average} />
+                                        </div>
+                                    </div>
+                                </li>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </ul>
     }
 
@@ -33,7 +47,7 @@ const SerieList = () => {
         <>
             <div className="container category">
 
-                <h2>Serie</h2>
+                <h2>Serie TV</h2>
             </div>
             <div>
                 {serie == undefined ? <p>Cerca le serie</p> : createList()}
